f60c67dae0f4732e155e1b7b179be5742292cbc1
Gijs Roge <rogegijs@gmail.com>
2022-12-18 17:18:01 +0100

    create reusable class

--- src/components/mobile-card.vue
+++ src/components/mobile-card.vue
@@ -4,18 +4,137 @@ export default {
 };
 </script>
 <script lang="ts" setup>
-import {
-  computed,
-  CSSProperties,
-  onMounted,
-  onUnmounted,
-  PropType,
-  ref,
-  watch,
-  watchEffect,
-} from "vue";
-import { clamp, MobileCard, soften } from "..";
+// const handleHeightInternal = ref(props.handleHeight || 50);
+// const cardRef = ref<HTMLElement | null>(null);
+// const handleRef = ref<HTMLElement | null>(null);
+// const isDragging = ref(false);
+// const startYPosition = ref(0);
+// const y = ref(0);
+// const treshHold = 50;
+// const isOpen = ref(false);
+// const heightOfContent = ref(props.maxDistance);
+// const firstRender = ref(true);
+// watch(y, () => {
+//   if (!isDragging.value) {
+//     isOpen.value = y.value > treshHold;
+//   }
+// });
+
+// const progress = computed(() => {
+//   return clamp(y.value / heightOfContent.value, 0, 1);
+// });
+
+// watch(progress, () => {
+//   emit("progress", progress.value);
+// });
+
+// watch(isOpen, () => {
+//   if (isOpen.value) startYPosition.value = heightOfContent.value;
+//   else startYPosition.value = 0;
+// });
+
+// onMounted(() => {
+//   mobileCard.mount();
+// });
+// onUnmounted(() => {
+//   mobileCard.unMount();
+// });
+
+// const startDrag = (e: MouseEvent | TouchEvent) => {
+//   document.documentElement.classList.add("overflow-hidden");
+//   if (e instanceof TouchEvent) {
+//     startYPosition.value =
+//       e.touches[0].clientY + (isOpen.value ? heightOfContent.value : 0);
+//   } else {
+//     startYPosition.value =
+//       e.clientY + (isOpen.value ? heightOfContent.value : 0);
+//   }
+
+//   isDragging.value = true;
+// };
+
+// const stopDrag = ({ force }: { force: boolean } = { force: false }) => {
+//   document.documentElement.classList.remove("overflow-hidden");
+//   if (!isDragging.value && !force) return;
+//   if (isOpen.value) {
+//     if (y.value < heightOfContent.value - treshHold) {
+//       y.value = 0;
+//     } else {
+//       y.value = heightOfContent.value;
+//     }
+//   } else {
+//     if (y.value >= treshHold) {
+//       y.value = heightOfContent.value;
+//     } else {
+//       y.value = 0;
+//     }
+//   }
+
+//   isOpen.value = y.value > treshHold;
+//   startYPosition.value = y.value;
+//   isDragging.value = false;
+// };
+
+// const whileDrag = (e: MouseEvent | TouchEvent) => {
+//   if (!isDragging.value) return;
+
+//   const clientY = e instanceof TouchEvent ? e.touches[0].clientY : e.clientY;
+//   y.value = soften(
+//     startYPosition.value - clientY,
+//     0,
+//     heightOfContent.value,
+//     heightOfContent.value
+//   );
+// };
+
+// const style = computed(() => {
+//   const styles: CSSProperties = {
+//     willChange: "transform",
+//     left: 0,
+//     right: 0,
+//     transition:
+//       isDragging.value || firstRender.value
+//         ? "none"
+//         : "transform 0.3s ease-out",
+//   };
+//   if (props.slideFrom === "top") {
+//     styles.top = 0;
+//     styles.transform = `translateY(${y.value}px)`;
+//   }
+//   if (props.slideFrom === "bottom") {
+//     styles.transform = `translateY(${
+//       (y.value - heightOfContent.value) * -1
+//     }px)`;
+//   }
+//   return styles;
+// });
+
+// // calculate height of cardRef using resize observer
+// const resizeObserver = new ResizeObserver((entries) => {
+//   if (!cardRef.value) return;
+//   const { blockSize: contentHeight } = entries[0].borderBoxSize[0];
+//   if (handleRef.value && !props.handleHeight)
+//     handleHeightInternal.value = handleRef.value.clientHeight;
+//   heightOfContent.value = contentHeight - handleHeightInternal.value;
+//   stopDrag({ force: true });
+//   if (firstRender.value && props.isOpen) {
+//     y.value = heightOfContent.value;
+//   }
+// });
+// watchEffect(() => {
+//   if (cardRef.value) {
+//     resizeObserver.observe(cardRef.value);
+//   }
+// });
+
+// onMounted(() => {
+//   setTimeout(() => (firstRender.value = false), 1);
+// });
+
+import { PropType, ref, watchEffect } from "vue";
+import { MobileCard } from "..";
 import handleAsset from "../assets/handle";
+const cardRef = ref<HTMLElement | null>(null);
 const props = defineProps({
   slideFrom: {
     type: Object as PropType<"top" | "bottom">,
@@ -50,152 +169,45 @@ const mobileCard = new MobileCard({
   dragEntireCard: props.dragEntireCard,
 });
 
-const emit = defineEmits(["progress"]);
+watchEffect(() => {
+  if (cardRef.value) mobileCard.cardElement = cardRef.value;
+});
 
-const handleHeightInternal = ref(props.handleHeight || 50);
-const cardRef = ref<HTMLElement | null>(null);
-const handleRef = ref<HTMLElement | null>(null);
-const isDragging = ref(false);
-const startYPosition = ref(0);
 const y = ref(0);
-const treshHold = 50;
-const isOpen = ref(false);
-const heightOfContent = ref(props.maxDistance);
-const firstRender = ref(true);
-watch(y, () => {
-  if (!isDragging.value) {
-    isOpen.value = y.value > treshHold;
-  }
-});
-
-const progress = computed(() => {
-  return clamp(y.value / heightOfContent.value, 0, 1);
-});
-
-watch(progress, () => {
-  emit("progress", progress.value);
-});
-
-watch(isOpen, () => {
-  if (isOpen.value) startYPosition.value = heightOfContent.value;
-  else startYPosition.value = 0;
-});
-
-onMounted(() => {
-  mobileCard.mount();
-});
-onUnmounted(() => {
-  mobileCard.unMount();
-});
-
-const startDrag = (e: MouseEvent | TouchEvent) => {
-  document.documentElement.classList.add("overflow-hidden");
-  if (e instanceof TouchEvent) {
-    startYPosition.value =
-      e.touches[0].clientY + (isOpen.value ? heightOfContent.value : 0);
-  } else {
-    startYPosition.value =
-      e.clientY + (isOpen.value ? heightOfContent.value : 0);
-  }
-
-  isDragging.value = true;
-};
-
-const stopDrag = ({ force }: { force: boolean } = { force: false }) => {
-  document.documentElement.classList.remove("overflow-hidden");
-  if (!isDragging.value && !force) return;
-  if (isOpen.value) {
-    if (y.value < heightOfContent.value - treshHold) {
-      y.value = 0;
-    } else {
-      y.value = heightOfContent.value;
-    }
-  } else {
-    if (y.value >= treshHold) {
-      y.value = heightOfContent.value;
-    } else {
-      y.value = 0;
-    }
-  }
-
-  isOpen.value = y.value > treshHold;
-  startYPosition.value = y.value;
-  isDragging.value = false;
-};
+const styles = ref({});
 
 const whileDrag = (e: MouseEvent | TouchEvent) => {
-  if (!isDragging.value) return;
-
-  const clientY = e instanceof TouchEvent ? e.touches[0].clientY : e.clientY;
-  y.value = soften(
-    startYPosition.value - clientY,
-    0,
-    heightOfContent.value,
-    heightOfContent.value
-  );
+  mobileCard.dragPosition(e);
 };
 
-const style = computed(() => {
-  const styles: CSSProperties = {
-    willChange: "transform",
-    left: 0,
-    right: 0,
-    transition:
-      isDragging.value || firstRender.value
-        ? "none"
-        : "transform 0.3s ease-out",
-  };
-  if (props.slideFrom === "top") {
-    styles.top = 0;
-    styles.transform = `translateY(${y.value}px)`;
-  }
-  if (props.slideFrom === "bottom") {
-    styles.transform = `translateY(${
-      (y.value - heightOfContent.value) * -1
-    }px)`;
-  }
-  return styles;
-});
+const startDrag = () => {
+  mobileCard.startDrag();
+};
 
-// calculate height of cardRef using resize observer
-const resizeObserver = new ResizeObserver((entries) => {
-  if (!cardRef.value) return;
-  const { blockSize: contentHeight } = entries[0].borderBoxSize[0];
-  if (handleRef.value && !props.handleHeight)
-    handleHeightInternal.value = handleRef.value.clientHeight;
-  heightOfContent.value = contentHeight - handleHeightInternal.value;
-  stopDrag({ force: true });
-  if (firstRender.value && props.isOpen) {
-    y.value = heightOfContent.value;
-  }
-});
-watchEffect(() => {
-  if (cardRef.value) {
-    resizeObserver.observe(cardRef.value);
-  }
-});
+const stopDrag = () => {
+  mobileCard.stopDrag({ force: false });
+};
 
-onMounted(() => {
-  setTimeout(() => (firstRender.value = false), 1);
-});
+const emit = defineEmits(["progress"]);
 </script>
 
 <template>
-  <div ref="cardRef" class="fixed" v-bind:style="style">
+  <div ref="cardRef" class="fixed">
     <div
       v-bind="$attrs"
       :style="{ marginTop: '0px !important' }"
       class="touch-none"
       @touchend="() => stopDrag()"
       @touchmove="whileDrag"
-      @mousedown="(event) => (dragEntireCard ? startDrag(event) : null)"
-      @touchstart="(event) => (dragEntireCard ? startDrag(event) : null)"
+      @mousedown="() => (dragEntireCard ? startDrag() : null)"
+      @touchstart="() => (dragEntireCard ? startDrag() : null)"
     >
       <div
         ref="handleRef"
-        @mousedown="(event) => (!dragEntireCard ? startDrag(event) : null)"
-        @touchstart="(event) => (!dragEntireCard ? startDrag(event) : null)"
+        @mousedown="() => (!dragEntireCard ? startDrag() : null)"
+        @touchstart="() => (!dragEntireCard ? startDrag() : null)"
       >
+        {{ styles }}
         <slot name="handle">
           <div
             class="cursor-move drag-handle h-[50px] relative w-full text-white flex items-center text-center justify-center"
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,7 @@
+import { animate } from "motion";
+
 export const clamp = (num: number, min: number, max: number) => {
   return Math.min(Math.max(num, min), max);
-};
-
-export const soften = (
-  num: number,
-  min: number,
-  max: number,
-  heightOfContent: number
-) => {
-  let position = num;
-  if (num > max) {
-    const rest = position - heightOfContent;
-    position -= rest * 0.6;
-  }
-  if (num < min) {
-    const rest = position;
-    position -= rest * 0.6;
-  }
-  return position;
-};
-
-export const getStyles = ({
-  isDragging,
-  firstRender,
-  heightOfContent,
-  slideFrom,
-  y,
-}: {
-  isDragging: boolean;
-  firstRender: boolean;
-  heightOfContent: number;
-  slideFrom: "top" | "bottom";
-  y: number;
-}) => {
-  const styles: any = {
-    willChange: "transform",
-    left: 0,
-    right: 0,
-    transition: isDragging || firstRender ? "none" : "transform 0.3s ease-out",
-  };
-  if (slideFrom === "top") {
-    styles.top = 0;
-    styles.transform = `translateY(${y}px)`;
-  }
-  if (slideFrom === "bottom") {
-    styles.transform = `translateY(${(y - heightOfContent) * -1}px)`;
-  }
-  return styles;
 };
 
 export class MobileCard {
@@ -55,6 +10,13 @@ export class MobileCard {
   isOpen: boolean;
   handleHeight: number | null;
   dragEntireCard: boolean;
+  isDragging: boolean;
+  y: number;
+  heightOfContent: number;
+  treshHold: number;
+  firstRender: boolean;
+  cardElement: HTMLElement | null;
+  movedScheduled: boolean;
 
   constructor({
     slideFrom,
@@ -74,15 +36,122 @@ export class MobileCard {
     this.isOpen = isOpen || false;
     this.handleHeight = handleHeight || null;
     this.dragEntireCard = dragEntireCard || false;
+    this.isDragging = false;
+    this.y = 0;
+    this.heightOfContent = 667;
+    this.treshHold = 50;
+    this.firstRender = true;
+    this.cardElement = null;
+    this.movedScheduled = false;
+  }
+
+  onChange(callback: () => void) {
+    callback();
   }
 
   mount() {
-    window.addEventListener("mouseup", () => stopDrag());
-    window.addEventListener("mousemove", whileDrag);
+    // window.addEventListener("mouseup", () => stopDrag());
+    // window.addEventListener("mousemove", whileDrag);
   }
 
   unMount() {
-    window.addEventListener("mouseup", () => stopDrag());
-    window.addEventListener("mousemove", whileDrag);
+    // window.addEventListener("mouseup", () => stopDrag());
+    // window.addEventListener("mousemove", whileDrag);
+  }
+
+  soften(num: number, min: number, max: number) {
+    let position = num;
+    if (num > max) {
+      const rest = position - this.heightOfContent;
+      position -= rest * 0.6;
+    }
+    if (num < min) {
+      const rest = position;
+      position -= rest * 0.6;
+    }
+    return position;
+  }
+
+  // whileDrag
+  dragPosition(e: MouseEvent | TouchEvent) {
+    console.log("drag");
+    if (!this.isDragging) return 0;
+
+    const clientY = e instanceof TouchEvent ? e.touches[0].clientY : e.clientY;
+    this.y = this.soften(this.y - clientY, 0, this.heightOfContent);
+
+    !this.movedScheduled &&
+      requestAnimationFrame(() => {
+        console.log("move");
+        this.move();
+      });
+    this.movedScheduled = true;
+  }
+
+  move() {
+    console.log(this.cardElement);
+    if (this.cardElement === null) return;
+
+    this.movedScheduled = false;
+    animate(
+      this.cardElement,
+      { y: this.y * -1 },
+      {
+        duration: 0,
+      }
+    );
+  }
+
+  // stopDrag
+  stopDrag({ force }: { force?: boolean } = { force: false }) {
+    document.documentElement.classList.remove("overflow-hidden");
+    if (!this.isDragging && !force) return;
+    if (this.isOpen) {
+      if (this.y < this.heightOfContent - this.treshHold) {
+        this.y = 0;
+      } else {
+        this.y = this.heightOfContent;
+      }
+    } else {
+      if (this.y >= this.treshHold) {
+        this.y = this.heightOfContent;
+      } else {
+        this.y = 0;
+      }
+    }
+
+    this.isOpen = this.y > this.treshHold;
+    this.isDragging = false;
+  }
+
+  // startDrag
+  startDrag() {
+    document.documentElement.classList.add("overflow-hidden");
+    this.isDragging = true;
+  }
+
+  // watch
+
+  // getStyles
+  getStyles() {
+    const styles: any = {
+      willChange: "transform",
+      left: 0,
+      right: 0,
+      transition:
+        this.isDragging || this.firstRender
+          ? "none"
+          : "transform 0.3s ease-out",
+    };
+    if (this.slideFrom === "top") {
+      styles.top = 0;
+      styles.transform = `translateY(${this.y}px)`;
+    }
+    if (this.slideFrom === "bottom") {
+      styles.transform = `translateY(${
+        (this.y - this.heightOfContent) * -1
+      }px)`;
+    }
+    return styles;
   }
 }
